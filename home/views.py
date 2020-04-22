68e1f63b434de65c39aa80c75a4fc8f16191d8e1
matt <matthewgstephenson@gmail.com>
2020-04-22 13:19:34 +0000

    working home template

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import Home
+
+def home(request):
+    projects = Home.objects.all()
+    context = {
+        'home': Home
+    }
+    return render(request, 'home.html', context)
+
+
